8f27e126c74685ff73bb76614f03361d42289b19
Isobek <ochilovisobek513@gmail.com>
2024-06-07 13:59:21 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css';
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+
+
+// import required modules
+import { EffectCards } from 'swiper/modules';
+
+function App() {
+  return (
+    <div className="App">
+      <Swiper
+        effect={'cards'}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="mySwiper"
+      >
+        <SwiperSlide>behruz</SwiperSlide>
+        <SwiperSlide>isobek</SwiperSlide>
+        <SwiperSlide>baxtishot</SwiperSlide>
+        <SwiperSlide>davron</SwiperSlide>
+        <SwiperSlide>ramazon</SwiperSlide>
+        <SwiperSlide>yahyo</SwiperSlide>
+        <SwiperSlide>javohiz</SwiperSlide>
+        <SwiperSlide>temuz</SwiperSlide>
+        <SwiperSlide>ramazon </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
+
+export default App;
